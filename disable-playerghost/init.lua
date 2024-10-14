01885b22053591a6734c1d3ad1fe977f8975afcc
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2024-10-14 20:06:40 +0200

    Set _DEBUG_DONT_SAVE_MAGIC_NUMBERS="1" magic num

--- disable-playerghost/init.lua
+++ disable-playerghost/init.lua
@@ -1,2 +1,6 @@
-ModTextFileSetContent("mods/disable-playerghost/m.xml",'<MagicNumbers APPARITION_MIN_BONES_REQUIRED="2000000000"/>')
+ModTextFileSetContent("mods/disable-playerghost/m.xml",[[
+    <MagicNumbers
+        _DEBUG_DONT_SAVE_MAGIC_NUMBERS="1"
+        APPARITION_MIN_BONES_REQUIRED="2000000000"/>
+]])
 ModMagicNumbersFileAdd("mods/disable-playerghost/m.xml")
